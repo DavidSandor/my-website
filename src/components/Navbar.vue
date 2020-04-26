0d9ac70ae670114b94f099cb52cbb375f0a0e50a
DavidSandor <sandor.david89@gmail.com>
2020-04-26 16:18:06 +0200

    Responsive navbar + bars menu update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,56 @@
 <template>
   <nav>
     <a class="navbar-brand" href="#">davidsandor</a>
-    <router-link class="router-link" to="/">Home</router-link> |
-    <router-link class="router-link" to="/references">References</router-link>
-    <router-link class="router-link" to="/about">About me</router-link>
-    <router-link class="router-link" to="/contact">Contact</router-link>
+    <div id="page-links" v-if="isPageLinksVisible" @click="routerLinkClickHandler">
+      <router-link class="router-link" to="/">Home</router-link>
+      <router-link class="router-link" to="/references">References</router-link>
+      <router-link class="router-link" to="/about">About me</router-link>
+      <router-link class="router-link" to="/contact">Contact</router-link>
+    </div>
+    <fa-icon id="links-menu" name="bars" @click="linksMenuClickHandler"></fa-icon>
   </nav>
 </template>
 
 <script>
-export default {
 
+import variables from '@/style/_variables.scss'
+
+export default {
+  data () {
+    return {
+      isPageLinksVisible: true,
+      screenWidth: window.innerWidth,
+    }
+  },
+  created () {
+    window.addEventListener('resize', this.windowResizeHandler)
+
+    if(this.screenWidth <= variables.screen_sm_width) {
+      this.isPageLinksVisible = false
+    }
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.windowResizeHandler)
+  },
+  methods: {
+    windowResizeHandler (event) {
+      this.screenWidth = event.target.innerWidth
+
+      if(this.screenWidth > variables.screen_sm_width) {
+        this.isPageLinksVisible = true
+      } else {
+        this.isPageLinksVisible = false
+      }     
+    },
+    linksMenuClickHandler () {
+      this.isPageLinksVisible = !this.isPageLinksVisible
+    },
+    routerLinkClickHandler () {
+      if(this.screenWidth <= variables.screen_sm_width) {
+        this.isPageLinksVisible = false
+      }
+    }
+  }
 }
 </script>
 
@@ -23,7 +63,7 @@ nav {
   margin-bottom: $general-space;
   text-align: right;
   background-color: $primary-bg-color;
-  opacity: 0.95;
+  opacity: 0.97;
   padding: 0 $general-space;
   box-shadow: $container-shadow;
 
@@ -32,6 +72,47 @@ nav {
     float: left;
     padding: 0;
     margin: 0;
+  }
+}
+
+#page-links {
+  @media all and (max-width: $screen-sm-width) {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: $navbar-height;
+    background-color: white;
+    color: $primary-theme-color;
+    font-weight: 600;
+    padding: $general-space;
+    box-shadow: inset 1px 4px 20px -5px rgba(49, 49, 49, 0.2);
+
+    & .router-link {
+      display: block;
+      max-width: 50%;
+      margin: 0 auto;
+      text-align: center;
+      color: inherit;
+    }
+
+    .router-link-exact-active {
+      color: inherit;
+      background-color: inherit;
+      font-weight: inherit;
+    }
+  }
+}
+
+#links-menu {
+  display: none;
+  height: $navbar-height;
+  width: 24px;
+  padding-left: 5px;
+  padding-right: 5px;
+  margin-right: -5px;
+  @media all and (max-width: $screen-sm-width) {
+    display: inline-block;
   }
 }
 
